3b19540b921faebbb512bb0516041ebc7d51efe6
Julian Martinez <julianmp0@gmail.com>
2023-01-07 16:48:22 -0500

    Initial Commit

---
+++ user/userApp.swift
@@ -0,0 +1,17 @@
+//
+//  userApp.swift
+//  user
+//
+//  Created by Julián Martinez on 7/01/23.
+//
+
+import SwiftUI
+
+@main
+struct userApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
